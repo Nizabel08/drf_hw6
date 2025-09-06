66bb2bde7b58257597c6cd3a836d767d582e989b
Nizabel08 <Nizabel2019>
2025-09-06 23:31:49 +0400

    add everything

--- projectHW/app/views.py
+++ projectHW/app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import *
 # Create your views here.
+
+def user_list(request) :
+    users = User.objects.all()
+    return render(request, 'users/user_list.html', {'users' : users})
+
+def user_detail(request, pk) :
+    user = get_object_or_404(User, pk= pk)
+    return render(request, 'users/user_detail.html', {'users' : user})
+
