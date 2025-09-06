98534e87efc0997de509f4283e044483539f86df
Nizabel08 <Nizabel2019>
2025-09-06 23:48:22 +0400

    add final version

--- projectHW/app/views.py
+++ projectHW/app/views.py
@@ -8,5 +8,5 @@ def user_list(request) :
 
 def user_detail(request, pk) :
     user = get_object_or_404(User, pk= pk)
-    return render(request, 'users/user_detail.html', {'users' : user})
+    return render(request, 'users/user_detail.html', {'user' : user})
 
